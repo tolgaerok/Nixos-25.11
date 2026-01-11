b1b94914b70cc558b485814b5d323ac19ea243aa
Tolga Erok <kingtolga@gmail.com>
2026-01-12 01:24:12 +0800

    (ツ)_/¯ Formatting issue -[ 12-01-2026 01:24 ]-

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -1,35 +1,32 @@
 { lib, pkgs, config, ... }:
 with lib;
 let cfg = config.drivers.nvidia;
+
 in {
-  options.drivers.nvidia = { 
-    enable = mkEnableOption "Enable Nvidia Drivers"; 
-  };
-  
+  options.drivers.nvidia = { enable = mkEnableOption "Enable Nvidia Drivers"; };
+
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "nvidia" ];
-    
+
     hardware.nvidia = {
-      nvidiaPersistenced = true;
       modesetting.enable = true;
+      nvidiaPersistenced = true;
+      nvidiaSettings = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
       powerManagement.enable = true;
       powerManagement.finegrained = false;
-      open = false;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
     hardware.graphics = {
       enable = true;
-      extraPackages = with pkgs; [
-        libva-utils
-      ];
+      extraPackages = with pkgs; [ libva-utils ];
     };
 
     # System packages
     environment.systemPackages = with pkgs; [
-      symbola
       egl-wayland
+      symbola
       vulkan-loader
       vulkan-tools
       vulkan-validation-layers
