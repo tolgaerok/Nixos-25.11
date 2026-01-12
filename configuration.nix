88487000901378e2f5b032f76e0958dd900b725c
Tolga Erok <kingtolga@gmail.com>
2026-01-12 10:28:12 +0800

    (ツ)_/¯ Added Specs into config -[ 12-01-2026 10:28 ]-

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,37 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running 'nixos-help').
-
 { config, pkgs, lib, ... }:
 
+# Tolga Erok
+# 3/11/2023
+# My personal NIXOS KDE configuration 
+# 
+#              ¯\_(ツ)_/¯
+#   ███▄    █     ██▓   ▒██   ██▒    ▒█████       ██████ 
+#   ██ ▀█   █    ▓██▒   ▒▒ █ █ ▒░   ▒██▒  ██▒   ▒██    ▒ 
+#  ▓██  ▀█ ██▒   ▒██▒   ░░  █   ░   ▒██░  ██▒   ░ ▓██▄   
+#  ▓██▒  ▐▌██▒   ░██░    ░ █ █ ▒    ▒██   ██░     ▒   ██▒
+#  ▒██░   ▓██░   ░██░   ▒██▒ ▒██▒   ░ ████▓▒░   ▒██████▒▒
+#  ░ ▒░   ▒ ▒    ░▓     ▒▒ ░ ░▓ ░   ░ ▒░▒░▒░    ▒ ▒▓▒ ▒ ░
+#  ░ ░░   ░ ▒░    ▒ ░   ░░   ░▒ ░     ░ ▒ ▒░    ░ ░▒  ░ ░
+#     ░   ░ ░     ▒ ░    ░    ░     ░ ░ ░ ▒     ░  ░  ░  
+#           ░     ░      ░    ░         ░ ░           ░  
+#  
+#------------------ HP EliteDesk 800 G4 SFF ------------------------
+
+# BLUE-TOOTH        REALTEK 5G
+# CPU	              Intel(R) Core(TM)  i7-8700 CPU @ 3.2GHz - 4.6Ghz (Turbo) x 6 (vPro)
+# i-GPU	            Intel UHD Graphics 630, Coffee Lake 
+# d-GPU             NVIDIA GeForce GTX 1650
+# MODEL             HP EliteDesk 800 G4 SFF
+# MOTHERBOARD	      Intel Q370 PCH-H—vPro
+# NETWORK	          Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz
+# RAM	              Maximum: 64 GB, DDR4-2666 (16 GB x 4)
+# STORAGE           256 GB, M.2 2280, PCIe NVMe SSD
+# EXPENSION SLOTS   (1) M.2 PCIe x1 2230 (for WLAN), (2) M.2 PCIe x4 2280/2230 combo (for storage)
+#                   (2) PCI Express v3.0 x1, (1) PCI Express v3.0 x16 (wired as x4), (1) PCI Express v3.0 x16
+# PSU               250W
+# SOURCE            https://support.hp.com/au-en/document/c06047207
+
+#---------------------------------------------------------------------
 {
   imports = [
     ./hardware-configuration.nix
