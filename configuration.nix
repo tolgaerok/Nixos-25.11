5133fbffefe3478c95ac9863926d5ef747db7318
Tolga Erok <kingtolga@gmail.com>
2026-02-08 16:30:17 +0800

    (ツ)_/¯ Ass swamping about -[ 08-02-2026 16:30 ]-

--- configuration.nix
+++ configuration.nix
@@ -39,32 +39,7 @@
 
   ];
 
-  # NVIDIA
-  drivers.nvidia.enable = true;
-
-  tweaks.io-optimization = {
-    enable = true;
-    scx.enable = true;
-    scx.scheduler = "rusty"; # or "lavd" for your VM workflow
-  };
-
-  # SYSTEM_VARIBLES
-  environment.variables = {
-    # wayland support
-    GDK_BACKEND = "wayland,x11"; # Prefer Wayland, fallback to X11
-    NIXOS_OZONE_WL = "1";
-    QT_QPA_PLATFORM = "wayland";
-  };
-
   # STATE_VERSION
   system.stateVersion = "25.11";
 }
 
-# Notes:
-# sudo chown -R $(whoami):$(id -gn) /etc/nixos && sudo chmod -R 777 /etc/nixos && sudo chmod +x /etc/nixos/* && export NIXPKGS_ALLOW_INSECURE=1
-
-# Vaccum logs
-# sudo journalctl --rotate && sudo journalctl --vacuum-time=1s && journalctl --disk-usage
-
-# Check boot times
-# systemd-analyze && systemctl status plymouth-start.service && systemd-analyze blame | head -20
--- modules/default.nix
+++ modules/default.nix
@@ -2,13 +2,14 @@
 
 {
   imports = [
-    ./boot
     # ./docker
+    ./boot
     ./firewall
     ./flatpaks
     ./fonts
     ./git
     ./hw-clock
+    ./io-optimization
     ./kde
     ./locale
     ./memory
@@ -24,6 +25,31 @@
     ./sound
     ./systemD
     ./users
-    ./io-optimization
   ];
+
+  # NVIDIA
+  drivers.nvidia.enable = true;
+
+  tweaks.io-optimization = {
+    enable = true;
+    scx.enable = true;
+    scx.scheduler = "rusty"; # or "lavd" for your VM workflow
+  };
+
+  # SYSTEM_VARIBLES
+  environment.variables = {
+    # wayland support
+    GDK_BACKEND = "wayland,x11"; # Prefer Wayland, fallback to X11
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+  };
 }
+
+# Notes:
+# sudo chown -R $(whoami):$(id -gn) /etc/nixos && sudo chmod -R 777 /etc/nixos && sudo chmod +x /etc/nixos/* && export NIXPKGS_ALLOW_INSECURE=1
+
+# Vaccum logs
+# sudo journalctl --rotate && sudo journalctl --vacuum-time=1s && journalctl --disk-usage
+
+# Check boot times
+# systemd-analyze && systemctl status plymouth-start.service && systemd-analyze blame | head -20
